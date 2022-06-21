667cbf6b5b7566a15383d1381efc019c54e3ada5
Kalrul <prizsabertler@gmail.comgit config --global user.email prizsabertler@gmail.com>
2022-06-21 09:13:24 -0700

    boo

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,6 +7,7 @@ Defines class Square with private attribute size
 class Square:
         """
         class Square definition
+
         Args:
             size : size of a side in square
         """
