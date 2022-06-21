2c4189c800cf0f69c8907929eb96410d5a8222c4
Kalrul <prizsabertler@gmail.comgit config --global user.email prizsabertler@gmail.com>
2022-06-21 09:38:13 -0700

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,40 +1,21 @@
 #!/usr/bin/python3
 """
-Module 3-square
-Defines class Square with private attribute size and public attribute area
+This is the "Square" module.
+
+This module provides a simple Square class with initialize size.
+Defaults size to 0. Raise error on invalid size inputs.
+Methods area returns size of area of the square.
 """
 
 
 class Square:
-    """
-    class Square definition
-
-    Args:
-        size (int): size of a side in square
-        
-    Functions:
-        __init__(self, size)
-        area(self)
-    """
-
+    """A class that defines a square by size and can compute area"""
     def __init__(self, size=0):
-        """
-        Initializes square
-        Attributes:
-            __size (int): size of a side of square, defaults to 0 if none
-        """
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
+        if type(size) != int:
+            raise TypeError("size must be an interger")
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
-        def area(self):
-            """
-            Calculates area of square
-
-            Returns:
-                area
-            """
-            return (self.__size)**2
+    def area(self):
+        return self.__size * self.__size
