56ee32b0bb24b0de03ea6dd3e56e82482f4d5c47
Kalrul <prizsabertler@gmail.comgit config --global user.email prizsabertler@gmail.com>
2022-06-28 01:39:57 -0700

    error cleared

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -35,7 +35,6 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-    
     @property
     def height(self):
         """Retrieves the height of a Rectangle instance."""
