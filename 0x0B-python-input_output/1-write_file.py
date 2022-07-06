74266ebc84d0eb370d10bf375859ade35ee85939
Kalrul <prizsabertler@gmail.comgit config --global user.email prizsabertler@gmail.com>
2022-07-06 07:01:54 -0700

    mema

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,29 +1,15 @@
 #!/usr/bin/python3
-"""Module 2-read_lines.
-Reads a certain number of lines from a file.
+"""
+Module 1-number_of_lines
+
+Contains function that returns number of lines in text file
 """
 
 
-def read_lines(filename="", nb_lines=0):
-    """Reads and prints nb_lines lines from filename.
-
-    Args:
-        - filename: name of the file
-        - nb_lines: number of lines to read
-    """
-
-    with open(filename) as f:
-        i = 0
-        count = 0
+def number_of_lines(filename=""):
+    """Return number of lines in text file"""
+    with open(filename, mode="r", encoding="utf-8") as f:
+        lines = 0
         for line in f:
-            count += 1
-        f.seek(0)
-        if nb_lines <= 0 or nb_lines >= count:
-                read_text = f.read()
-                print(read_text, end="")
-        else:
-            for line in f:
-                print(line, end='')
-                i += 1
-                if i == nb_lines:
-                    break
+            lines += 1
+    return lines
